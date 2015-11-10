9250b9e1ff86b06543572f5d8c9942976d0406e3
ctinaa <christina.fcook@yahoo.com>
2015-11-09 23:55:58 -0800

    :bowtie: 11.9.15 Problem 1.2 Completed

--- chapter1_notes.py
+++ chapter1_notes.py
@@ -2,7 +2,11 @@
 --------------------
 Notes on Hash Tables 
 --------------------
-
+Based off CS50 
+- Make use of all info provided by key 
+- Spread evenly across hash table; reduce collision 
+- Generate different hash values for similar keys 
+- Speedy insertion, delection and lookup == deploy quick operations to minimize run time
 Pseudo Code 
 
 
